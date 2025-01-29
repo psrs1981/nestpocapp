7fb10abff3f63c198b92a5453ec018deed5aa26e
psrs1981 <psrs1981@gmail.com>
2025-01-29 14:33:55 -0500

    Update app.controller.spec.ts

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Welcome to the World of Hanuman!');
+      expect(appController.getHello()).toBe('Welcome to the World of Hanuma!');
     });
   });
 });
